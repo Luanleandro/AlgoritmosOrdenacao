422b1f0374f853aa13b68c63bdfd900522ab086f
LuanLeandro <luan.barbosacruz@gmail.com>
2023-10-22 13:38:54 -0300

    BurbleSort

---
+++ BurbleSort/burble.js
@@ -0,0 +1,12 @@
+function burbleSort(array) {
+  for (let i = 0; i <= array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+}
+
+console.log(burbleSort([10, 2, 18, 5, 19]));
