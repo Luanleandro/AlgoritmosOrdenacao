3de4b994d3feea62cae55c33def89ec6fb9aea69
LuanLeandro <luan.barbosacruz@gmail.com>
2023-10-26 16:24:04 -0300

    ShellSort

---
+++ ShellSort/ShellSort.js
@@ -0,0 +1,18 @@
+function shellShort(array) {
+  let n = array.length;
+  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    for (let i = gap; i < n; i += 1) {
+      let temp = array[i];
+      let j;
+      for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+        array[j] = array[j - gap];
+      }
+      array[j] = temp;
+    }
+  }
+  return array;
+}
+
+vetor = [5, 1, 9, 2, 10, 12, 550];
+shellShort(vetor);
+console.log(vetor)
